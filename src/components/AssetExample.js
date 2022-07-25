5e63f279a1d358de26552de6036ab8f1c6f5f2cc
Matheus <mateeus.soarees@outlook.com>
2022-07-25 12:45:34 -0300

    Primeiro app com react native

---
+++ src/components/AssetExample.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex:1,
+  },
+});
